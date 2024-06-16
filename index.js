de577f25b5aac201ef01907f89e1d9a3902c96b5
Omkar <Omkar861856@gmail.com>
2024-06-17 02:34:58 +0530

    cors

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ import signUpRouter from './routes/signup.js';
 dotenv.config();
 export const app = express();
 
-// const corsOptions = {
-//   origin: 'http://localhost:3030', // Replace with your frontend URL
-//   optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-// };
-
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3030', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use(bodyparser.urlencoded({ extended: false }));
