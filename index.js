7a13d3f9750bd4c510c9fa25159030b61ef91776
Omkar <Omkar861856@gmail.com>
2024-06-17 17:26:45 +0530

    minor changes

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ import updateUserRouter from './routes/update-user.js';
 dotenv.config();
 export const app = express();
 
-const corsOptions ={
-  origin:'http://localhost:3030', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//   origin:'http://localhost:3030', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+app.use(cors());
 app.use(express.json())
 
 app.use(bodyparser.urlencoded({ extended: false }));
